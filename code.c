2299c88c18490869508c1b2c405ae752ef692060
usr-undeleted <zephimhere@gmail.com>
2026-03-26 18:48:58 -0300

    small bit of commenting

--- code.c
+++ code.c
@@ -32,9 +32,9 @@ const char *playerColors[2] = {
 };
 
 const int winCombinations[8][3] = {
-    {0,1,2}, {3,4,5}, {6,7,8},
-    {0,3,6}, {1,4,7}, {2,5,8},
-    {0,4,8}, {2,4,6}
+    {0,1,2}, {3,4,5}, {6,7,8}, // horizontal
+    {0,3,6}, {1,4,7}, {2,5,8}, // vertical
+    {0,4,8}, {2,4,6} // diagonal
 };
 
 int checkWin(int player) {
